db03886afa735701988ef96d9a3c5ee192c08e39
A. Yasin Akalin <ayasinakalin@gmail.com>
2019-03-20 01:58:45 +0300

    Create definitions.py at root directory
    Contains init() function to be used at the first execution of the program. It creates a global variable pointing to ROOT DIRECTORY and changes active directory of active shell session to it.

---
+++ src/definitions.py
@@ -0,0 +1,22 @@
+# definition.py
+# from pathlib import Path
+
+
+def salutation():
+    print("Hello")
+
+
+def init():
+    from pathlib import Path
+    import os
+
+    salutation()
+
+    # DIR_SRC points to project directory which is CS210_Project_1/src
+    # and can be reached from definitions.DIR_SRC after
+    # running definitions.init() just once
+    global DIR_SRC
+    DIR_SRC = Path(__file__).parent
+
+    # Changes active directory to project directory
+    os.chdir(DIR_SRC)
