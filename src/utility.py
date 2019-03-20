0958c48e870621d1663ccdc8427fbc8928893a16
A. Yasin Akalin <ayasinakalin@gmail.com>
2019-03-20 18:57:15 +0300

    Update 'utility.py' with countdown()

--- src/utility.py
+++ src/utility.py
@@ -69,3 +69,29 @@ def installRequirements():
 def rerun(command):
     response = sp.run(command, shell=True)
     return checkResponse(response, verbose=False)
+
+
+def countdown(seconds=None):
+    import time
+
+    while True:
+        while not seconds or type(seconds) is not int:
+            uin = input(">> ")
+            try:
+                seconds = abs(int(uin))
+            except KeyboardInterrupt:
+                break
+            except:
+                print("Not a number!")
+
+        for second in reversed(range(seconds)):
+            m, s = divmod(second, 60)
+            h, m = divmod(m, 60)
+            time_left = str(h).zfill(2) \
+                + ":" + str(m).zfill(2) \
+                + ":" + str(s).zfill(2)
+            # '\r' helps to print over the same line
+            print(time_left + "\r", end="")
+            time.sleep(1)  # Pauses execution for 1 second so countdown works
+        print()
+        break
